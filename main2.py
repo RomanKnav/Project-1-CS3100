52324ae2d85a56a65aa97411c6eabac5debbe493
roman <ronnoverro@gmail.com>
2024-09-05 00:54:09 -0700

    code overhaul

--- main2.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def load_data(filename)
-    myList = []
-    with open(filename) as data:
-        countries_data = csv.reader(data, delimiter=',')
-
-
-# TODO: store the entries in a list
