96fe7a653c2e5bc38d679f78bb7ff9a68b91ffb4
roman <ronnoverro@gmail.com>
2024-09-06 00:31:13 -0700

    working on medals

--- fileReader2.py
+++ fileReader2.py
@@ -4,7 +4,8 @@ import math
 # countries: 'kaggle files/data/countries-table.csv'
 # olympics:  'kaggle files/data/Olympics 2024.csv'
 
-medals = ["Gold", "Silver", "Bronze"]
+# "Gold", "Silver", "Bronze"
+medals = ["gold medals", "silver medals", "bronze medals"]
 
 # this works:
 # returns a list of a given country's stats:
@@ -37,6 +38,38 @@ def allCountries():
             countries.append(row[0])
         
         return countries
+    
+def getMedals(country, medal):
+    # by this point, medal is fully formatted.
+
+    with open('kaggle files/data/countries-table.csv') as file1:
+        csvReader = csv.reader(file1, delimiter=',', quotechar='|')
+
+        """
+        Gold medal: 3
+        Silver medal: 4 
+        Bronze medal: 5
+        """
+
+        medalIndex = None
+
+        # medal of any type:
+        xMedals = 0
+
+        match medal:
+            case "Gold":
+                medalIndex = 3
+            case "Silver":
+                medalIndex = 4
+            case "Bronze":
+                medalIndex = 5
+
+        for row in csvReader:
+            if (row[2] == country):
+                xMedals += row[medalIndex]
+
+        return xMedals
+
 
 """ country keys:   
     0: country name
@@ -50,6 +83,10 @@ def allCountries():
 
     olympics key:
 """ 
+
+""""Queries:
+    medals, athletics
+"""
 
 def loop():
     while True:
@@ -89,9 +126,6 @@ def loop():
             print("input query:")
             query = input()
 
-            # check if query is a country, medal type, or sport:
-            # looks like I'll have to make lists for medals and sports
-
             ##################SECOND INPUT#####################
 
             # we have a country (if statement works):
@@ -99,14 +133,16 @@ def loop():
                 country2 = countryStats(query)
 
                 country2Size = country2[3]
-                country2Pop = country2[16]
 
                 # these are LISTS:
                 biggerCountry = None
                 smallerCountry = None
 
+                # these should ALSO be lists:
                 biggerCountryPop = None
                 smallerCountryPop = None
+                biggerCountryPopName = None
+                smallerCountryPopName = None
 
                 if (country1Size > country2Size):
                     biggerCountry = country1
@@ -115,33 +151,42 @@ def loop():
                     biggerCountry = country2
                     smallerCountry = country1
 
-                if (biggerCountry[16] > smallerCountry[16]):
-                    print(biggerCountry[0], "population:", biggerCountry[16])
-                    print(smallerCountry[0], "population:", smallerCountry[16])
-                    biggerCountryPop = biggerCountry[16]
-                    smallerCountryPop = smallerCountry[16]
+                # TODO: get NAME of bigger population country
+
+                if (int(biggerCountry[16]) > int(smallerCountry[16])):
+                    biggerCountryPopName = biggerCountry[0]
+                    smallerCountryPopName = smallerCountry[0]
+                    biggerCountryPop = int(biggerCountry[16])
+                    smallerCountryPop = int(smallerCountry[16])
+                    print(biggerCountryPopName, "has a bigger population than", smallerCountryPopName)
                 else:
-                    print(smallerCountry, "has the bigger population")
-                    biggerCountryPop = smallerCountry[16]
-                    smallerCountryPop = biggerCountry[16]
+                    # THIS keeps running:
+                    biggerCountryPopName = smallerCountry[0]
+                    smallerCountryPopName = biggerCountry[0]
+                    biggerCountryPop = int(smallerCountry[16])
+                    smallerCountryPop = int(biggerCountry[16])
 
                 size_quotient = math.floor(int(biggerCountry[3]) / 
                                            int(smallerCountry[3])
                                           )
                 
-                pop_quotient = math.floor(int(biggerCountryPop) / 
-                                          int(smallerCountryPop
-                                        ))
+                pop_quotient = math.floor(biggerCountryPop / 
+                                          smallerCountryPop
+                                        )
 
                 if (size_quotient == 1):
-                    print("You can fit %s %s in %s" % (size_quotient, smallerCountry[0], biggerCountry[0]))
+                    print("\nYou can fit %s %s in %s" % (size_quotient, smallerCountry[0], biggerCountry[0]))
                 else:
-                    print("You can fit %s %ss in %s" % (size_quotient, smallerCountry[0], biggerCountry[0]))
+                    print("\nYou can fit %s %ss in %s" % (size_quotient, smallerCountry[0], biggerCountry[0]))
 
-                print("\nthere are %s people in %s for every person in %s" % (pop_quotient, biggerCountry[0], smallerCountry[0]))
+                print("\nthere are %s people in %s for every person in %s\n" % (pop_quotient, biggerCountryPopName, smallerCountryPopName))
 
+            # compare sizes, populations, growth rates. 
+            elif (query in medals):
+                keyWord = (query.split(maxsplit=1)[0]).capitalize()
+                # print(keyWord)
+                print("this country possess", getMedals(country, keyWord), keyWord, "medals")
 
-                # compare sizes, populations, growth rates. 
 
         else:
             # for first country:
